a7e9974e62b215858087235d9e09c7cd8241b271
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2024-09-14 16:49:08 -0500

    setup react and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 
